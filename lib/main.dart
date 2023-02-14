b952d3139c573d13b687b89b5fb5a0d10346e15b
Rhythm Bhandari <rhythmbhandari@outlook.com>
2023-02-15 01:26:18 +0545

    Example8

--- lib/main.dart
+++ lib/main.dart
@@ -36,63 +36,6 @@ class MyApp extends StatelessWidget {
 const url =
     'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg';
 
-enum Action {
-  rotateLeft,
-  rotateRight,
-  moreVisible,
-  lessVisible,
-}
-
-@immutable
-class State {
-  final double rotationDeg;
-  final double alpha;
-
-  const State({
-    required this.rotationDeg,
-    required this.alpha,
-  });
-
-  const State.zero()
-      : rotationDeg = 0.0,
-        alpha = 1.0;
-
-  State rotateRight() => State(
-        alpha: alpha,
-        rotationDeg: rotationDeg + 10.0,
-      );
-
-  State rotateLeft() => State(
-        alpha: alpha,
-        rotationDeg: rotationDeg - 10.0,
-      );
-
-  State increaseAlpha() => State(
-        alpha: min(alpha + 0.1, 1.0),
-        rotationDeg: rotationDeg,
-      );
-
-  State decreaseAlpha() => State(
-        alpha: max(alpha - 0.1, 0.0),
-        rotationDeg: rotationDeg,
-      );
-}
-
-State reducer(State oldState, Action? action) {
-  switch (action) {
-    case Action.rotateLeft:
-      return oldState.rotateLeft();
-    case Action.rotateRight:
-      return oldState.rotateRight();
-    case Action.moreVisible:
-      return oldState.increaseAlpha();
-    case Action.lessVisible:
-      return oldState.decreaseAlpha();
-    case null:
-      return oldState;
-  }
-}
-
 class MyHomePage extends HookWidget {
   const MyHomePage({
     super.key,
@@ -100,113 +43,32 @@ class MyHomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final store = useReducer<State, Action?>(reducer,
-        initialState: const State.zero(), initialAction: null);
+    final state = useAppLifecycleState();
+    // final AppLifecycleState lifeCycle;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Home page',
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              RotateLeftButton(store: store),
-              RotateRightButton(store: store),
-              IncreaseAlphaButton(store: store),
-              DecreaseAlphaButton(store: store),
-            ],
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          Opacity(
-            opacity: store.state.alpha,
-            child: RotationTransition(
-                turns: AlwaysStoppedAnimation(
-                  store.state.rotationDeg / 360.0,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Opacity(
+          opacity: state == AppLifecycleState.resumed ? 1.0: 0.0,
+          child: Container(
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                color: Colors.black.withAlpha(
+                  100,
                 ),
-                child: Image.network(url)),
-          )
-        ],
+                spreadRadius: 10,
+              )
+            ]),
+            child: Image.network(url),
+          ),
+        ),
       ),
-    );
-  }
-}
-
-class DecreaseAlphaButton extends StatelessWidget {
-  const DecreaseAlphaButton({
-    super.key,
-    required this.store,
-  });
-
-  final Store<State, Action?> store;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        store.dispatch(Action.lessVisible);
-      },
-      child: const Text('- Alpha'),
-    );
-  }
-}
-
-class IncreaseAlphaButton extends StatelessWidget {
-  const IncreaseAlphaButton({
-    super.key,
-    required this.store,
-  });
-
-  final Store<State, Action?> store;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        store.dispatch(Action.moreVisible);
-      },
-      child: const Text('+ Alpha'),
-    );
-  }
-}
-
-class RotateRightButton extends StatelessWidget {
-  const RotateRightButton({
-    super.key,
-    required this.store,
-  });
-
-  final Store<State, Action?> store;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        store.dispatch(Action.rotateRight);
-      },
-      child: const Text('Rotate Right'),
-    );
-  }
-}
-
-class RotateLeftButton extends StatelessWidget {
-  const RotateLeftButton({
-    super.key,
-    required this.store,
-  });
-
-  final Store<State, Action?> store;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        store.dispatch(Action.rotateLeft);
-      },
-      child: const Text('Rotate Left'),
     );
   }
 }
