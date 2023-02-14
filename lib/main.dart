0335b9b3bad923595f24d24bf2916dc826eaedd4
Rhythm Bhandari <rhythmbhandari@outlook.com>
2023-02-14 23:18:50 +0545

    Simple hooks example 1

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+Stream<String> getTime() => Stream.periodic(
+      const Duration(
+        seconds: 1,
+      ),
+      (_) => DateTime.now().toIso8601String(),
+    );
+
+class MyHomePage extends HookWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final dateTime = useStream(
+      getTime(),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          dateTime.data ?? 'Home page',
+        ),
+      ),
+    );
+  }
+}
